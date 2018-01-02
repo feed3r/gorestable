31a743b8ac7df36d44e05bc006be603825940bf9
Francesco Romani <fromani@gmail.com>
2018-01-02 21:40:04 +0100

    rest: initial endpoints
    set routes and endpoints to be implemented in v0.1

--- main.go
+++ main.go
@@ -2,9 +2,9 @@ package main
 
 import (
     "fmt"
-    "html"
     "log"
     "net/http"
+    "strconv"
 
     "github.com/gorilla/mux"
 )
@@ -12,10 +12,35 @@ import (
 func main() {
 
     router := mux.NewRouter().StrictSlash(true)
-    router.HandleFunc("/", Index)
+    router.HandleFunc("/tables", TablesIndex)
+    router.HandleFunc("/reservations", ReservationsIndex)
+    router.HandleFunc("/users/{user}", UserByName)
+    router.HandleFunc("/days", DaysIndex)
+    router.HandleFunc("/days/{offset}", DayByOffset)
+
     log.Fatal(http.ListenAndServe(":8080", router))
 }
 
-func Index(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+func TablesIndex(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintln(w, "Endpoint to query today's table state")
+}
+
+func ReservationsIndex(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintln(w, "Endpoint to submit a table reservation")
+}
+
+func UserByName(w http.ResponseWriter, r *http.Request) {
+    vars := mux.Vars(r)
+    fmt.Fprintf(w, "Endpoint to query all the table reservations for user %s\n", vars["user"])
+}
+
+
+func DaysIndex(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintln(w, "Endpoint to query all the table reservations by day")
+}
+
+func DayByOffset(w http.ResponseWriter, r *http.Request) {
+    vars := mux.Vars(r)
+    offset, _ := strconv.Atoi(vars["offset"]) // TODO handle error properly
+    fmt.Fprintf(w, "Endpoint to query all the table reservations for T+%d\n", offset)
 }
