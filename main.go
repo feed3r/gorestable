45036528e5bf9c7e1461121266fa627f170cf05f
feed3r <feeder81@gmail.com>
2018-01-06 19:42:52 +0100

    Refactored main
    Simplified main in order to use the router object previously defined

--- main.go
+++ main.go
@@ -3,18 +3,11 @@ package main
 import (
 	"log"
 	"net/http"
-
-	"github.com/gorilla/mux"
 )
 
 func main() {
 
-	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/tables", TablesIndex)
-	router.HandleFunc("/reservations", ReservationsIndex)
-	router.HandleFunc("/users/{user}", UserByName)
-	router.HandleFunc("/days", DaysIndex)
-	router.HandleFunc("/days/{offset}", DayByOffset)
+	router := NewRouter()
 
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
