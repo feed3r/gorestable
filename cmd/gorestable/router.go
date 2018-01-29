28f94a0185b4f9afc195f2c259322ea16b17646f
feed3r <feeder81@gmail.com>
2018-01-29 22:07:25 +0100

    HandlerLogger function moved and unexported
    HandlerLogger function has been removed from logger.go file.
    The function has been inlined and unexported, too.

--- cmd/gorestable/router.go
+++ cmd/gorestable/router.go
@@ -1,10 +1,29 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/mux"
 )
+
+func handlerLogger(inner http.Handler, name string) http.Handler {
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+
+		inner.ServeHTTP(w, r)
+
+		log.Printf(
+			"%s\t%s\t%s\t%s",
+			r.Method,
+			r.RequestURI,
+			name,
+			time.Since(start),
+		)
+	})
+}
 
 func NewRouter() *mux.Router {
 
@@ -14,7 +33,7 @@ func NewRouter() *mux.Router {
 		var handler http.Handler
 
 		handler = route.HandlerFunc
-		handler = HandlerLogger(handler, route.Name)
+		handler = handlerLogger(handler, route.Name)
 
 		router.
 			Methods(route.Method).
